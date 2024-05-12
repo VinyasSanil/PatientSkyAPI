16eefaca9ca7ea688192a4bae6baa8b7bb6dd17f
VinyasSanil <vinyassanil@gmail.com>
2024-05-12 17:27:29 +0530

    Changes

--- PatientSkyAPI/Program.cs
+++ PatientSkyAPI/Program.cs
@@ -1,13 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,8 +16,6 @@ app.UseHttpsRedirection();
 app.MapPost("/findAvailableTime", (AppointmentRequest request) =>
 {
     AppointmentHandler appointmentHandler = new AppointmentHandler();
-    //DateRanges dateRanges = appointmentHandler.SplitDateRange(request.PeriodToSearch);
-    //List<Calendar> calendarNames = appointmentHandler.GetCalendarRecord(request.CalendarIds);
     List<AvailableTimes> AvailableTimeList = appointmentHandler.GetAvailableTimes(request.CalendarIds, request.PeriodToSearch, request.Duration, request.TimeSlotType);
     
     return AvailableTimeList;
